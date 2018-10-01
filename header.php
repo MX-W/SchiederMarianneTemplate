b9dbd37da5a31e91f9a2a620bacb00c442135db5
118620 <u93266883@cbi-bs1.server.lan>
2018-10-01 14:42:41 +0200

    ok

--- header.php
+++ header.php
@@ -45,7 +45,10 @@ $css_classes = 'container-fluid';
         <div class="row menu-container">
             <div class="col-lg-2"></div>
             <div class="col-lg-10">
-	            <?php wp_nav_menu(array('theme_location' => 'header-menu', 'container_class' => 'menu-header')); ?>
+                <a href="javascript:void(0);" class="icon" onclick="responsiveNav()">
+                    <i class="fa fa-bars"></i>
+                </a>
+                <?php wp_nav_menu(array('theme_location' => 'header-menu', 'container_class' => 'menu-header')); ?>
             </div>
         </div>
 
